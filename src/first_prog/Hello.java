bc1e45ae501eafb5fe81349dadc3ffc934378952
Prasnna <Prasnna@192.168.0.25>
2021-11-17 14:21:07 -0500

    normal checking

--- src/first_prog/Hello.java
+++ src/first_prog/Hello.java
@@ -11,5 +11,6 @@ System.out.println("My first Webhook trial");
 System.out.println("My second Webhook trial");
 System.out.println("want to chech again");
 System.out.println("check");
+System.out.println("class check");
 	}
 }
